8c5a990bcfd6ec0948e42ed9e8a2e0766d41ea89
aliu292 <andyycliu@yahoo.com>
2022-11-03 22:28:57 +0800

    Changed alpha video

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     const {renderer, scene, camera} = mindarThree;
 
-    const video = await loadVideo('./videoEdits/Mist Ink Alpha_1_1.webm');
+    const video = await loadVideo('./videoEdits/Mist Ink AInc.webm');
     const texture = new THREE.VideoTexture({video: video, format : RGBAFormat});
 
     const geometry = new THREE.PlaneGeometry(1.5, 748/600*1.5);
