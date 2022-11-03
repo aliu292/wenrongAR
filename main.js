9e42eaae5b1e9595c86e848bb8063bdc1b0ace8c
aliu292 <andyycliu@yahoo.com>
2022-11-03 21:15:16 +0800

    link

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-import {loadGLTF, loadVideo} from "/libs/loader.js";
+import {loadGLTF, loadVideo} from "./libs/loader.js";
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '/arTargets/targets.mind',
+      imageTargetSrc: './arTargets/targets.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
-    const video = await loadVideo('/videoEdits/chi-wild.mp4');
+    const video = await loadVideo('./videoEdits/chi-wild.mp4');
     const texture = new THREE.VideoTexture(video);
 
     const geometry = new THREE.PlaneGeometry(1, 204/480);
